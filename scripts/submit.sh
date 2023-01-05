5b95859083a3b70a0315a7788adc9d62918bc0b8
Fukiwake <tak0203753@gmail.com>
2023-01-05 23:42:21 +0000

    スクリプトの修正

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,4 +3,5 @@
 # $1 contest_name
 # $2 problem_name
 
-acc submit /workspace/code/$1/$2/$2.py
+cd /workspace/code/$1/$2
+acc submit code.py
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,4 @@
 # $1 contest_name
 # $2 problem_name
 
-oj t -c "python3 /workspace/code/$1/$2/$2.py" -d /workspace/code/$1/$2/tests/
+oj t -c "python3 /workspace/code/$1/$2/code.py" -d /workspace/code/$1/$2/tests/
