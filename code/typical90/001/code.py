9a0ba807ea60b2f9f0f36525d1ed193ad883f4d9
Fukiwake <tak0203753@gmail.com>
2023-07-24 21:50:38 +0900

    001

--- code/typical90/001/code.py
+++ code/typical90/001/code.py
@@ -1,7 +1,7 @@
 def main():
-    x = int(input())
-    y = map(int, input().split())
-    z = list(map(int, input().split()))
+    N, L = map(int, input().split())
+    K = int(input())
+    A = list(map(int, input().split()))
 
 if __name__ == "__main__":
     main()
