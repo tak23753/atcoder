784dae707de0d1673df9d749bcae6d27a621e59a
Takashi Fukiwake <takashifukiwake@PC085.local>
2023-07-28 12:29:23 +0900

    スニペットを修正

--- snippet.py
+++ snippet.py
@@ -1,7 +1,3 @@
-def main():
-    x = int(input())
-    y = map(int, input().split())
-    z = list(map(int, input().split()))
-
-if __name__ == "__main__":
-    main()
+x = int(input())
+y = map(int, input().split())
+z = list(map(int, input().split()))
