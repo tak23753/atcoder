ec03964bbc4cb0633183ca2a52dc64642436da49
Takashi Fukiwake <takashifukiwake@PC085.local>
2023-07-28 12:30:10 +0900

    競プロ典型 90 問 001

--- code/typical90/001/code.py
+++ code/typical90/001/code.py
@@ -1,7 +1,28 @@
-def main():
-    N, L = map(int, input().split())
-    K = int(input())
-    A = list(map(int, input().split()))
+N, L = map(int, input().split())
+K = int(input())
+A = list(map(int, input().split()))
 
-if __name__ == "__main__":
-    main()
+def check(x):
+    count = 0
+    prev = 0
+
+    for i in range(N):
+        if A[i] - prev >= x:
+            count += 1
+            prev = A[i]
+
+    if L - prev >= x:
+        count += 1
+
+    return (count >= K + 1)
+
+left, right = -1, L + 1
+while right - left > 1:
+    mid = (left + right) // 2
+
+    if check(mid):
+        left = mid
+    else:
+        right = mid
+
+print(left)
