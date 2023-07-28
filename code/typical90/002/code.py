82fce2dad169b1d1618587516a34615b1bf70ea3
Takashi Fukiwake <takashifukiwake@PC085.local>
2023-07-28 12:30:57 +0900

    競プロ典型 90 問 002

--- code/typical90/002/code.py
+++ code/typical90/002/code.py
@@ -1,7 +1,21 @@
-def main():
-    x = int(input())
-    y = map(int, input().split())
-    z = list(map(int, input().split()))
 
-if __name__ == "__main__":
-    main()
+from itertools import product
+
+def check(str):
+    score = 0
+
+    for char in str:
+        if char == '(':
+            score += 1
+        elif char == ')':
+            score -= 1
+
+        if score < 0:
+            return False
+
+    return (score == 0)
+
+N = int(input())
+for pro in product(('(', ')'), repeat=N):
+        if (check(pro)):
+            print(*pro, sep='')
